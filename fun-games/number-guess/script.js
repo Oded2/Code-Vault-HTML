fe805988b39bfdf265751cbd65784b4d61320015
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-06-11 23:12:00 +0300

    In progress adding a new game

--- fun-games/number-guess/script.js
+++ fun-games/number-guess/script.js
@@ -1,5 +1,6 @@
 let winningNum;
 let success = false;
+let tries = 0;
 function randomNum(min, max) {
   let x = Math.floor(Math.random() * max + min);
   return x;
@@ -8,6 +9,7 @@ function randomNum(min, max) {
 function getNumber() {
   const min = parseInt(document.getElementById("min-num").value);
   const max = parseInt(document.getElementById("max-num").value);
+
   if (isNaN(min) || isNaN(max)) {
     alert("Cannot have empty imputs");
     return;
@@ -15,9 +17,12 @@ function getNumber() {
     alert("Your minimum number cannot be higher than your maximum number");
     return;
   }
+
   winningNum = randomNum(min, max);
+  document.getElementById("random-number").innerText = winningNum;
 }
 function check() {
+  tries += 1;
   console.log(winningNum);
   const userNum = document.getElementById("userNum").value;
   let lowerOrHigher = document.getElementById("lowerOrHigher");
