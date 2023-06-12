af9be5cb901d8ffa0952be49beea617d339a628c
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-06-13 00:09:16 +0300

    Game coming along

--- fun-games/number-guess/script.js
+++ fun-games/number-guess/script.js
@@ -1,6 +1,6 @@
 let winningNum;
-let success = false;
 let tries = 0;
+let attemptedNums = [];
 function randomNum(min, max) {
   let x = Math.floor(Math.random() * max + min);
   return x;
@@ -11,7 +11,7 @@ function getNumber() {
   const max = parseInt(document.getElementById("max-num").value);
 
   if (isNaN(min) || isNaN(max)) {
-    alert("Cannot have empty imputs");
+    alert("Input must have numbers");
     return;
   } else if (min > max) {
     alert("Your minimum number cannot be higher than your maximum number");
@@ -19,19 +19,69 @@ function getNumber() {
   }
 
   winningNum = randomNum(min, max);
-  document.getElementById("random-number").innerText = winningNum;
+  document.getElementById("min-num").disabled = true;
+  document.getElementById("max-num").disabled = true;
+  document.getElementById("start-btn").disabled = true;
+  document.getElementById("start-btn").className =
+    "btn btn-secondary fs-5 w-25";
+  document.getElementById("check-btn").disabled = false;
+  document.getElementById("check-btn").classList.remove("btn-secondary");
+  document.getElementById("check-btn").classList.add("btn-primary");
+  document.getElementById("userNum").disabled = false;
+  document.getElementById("reveal-btn").disabled = false;
+  document.getElementById("reveal-btn").classList.remove("btn-secondary");
+  document.getElementById("reveal-btn").classList.add("btn-warning");
 }
 function check() {
   tries += 1;
-  console.log(winningNum);
+  const attempted = document.getElementById("attempted");
   const userNum = document.getElementById("userNum").value;
-  let lowerOrHigher = document.getElementById("lowerOrHigher");
+  const div = document.getElementById("numInfo");
+  const trydiv = document.getElementById("tryDiv");
+  const attempts = document.getElementById("tries");
+  const lowerOrHigher = document.getElementById("lowerOrHigher");
+  if (userNum == "") {
+    return;
+  }
+  div.classList.remove("bg-secondary");
+  attempts.innerText = "Attempts: " + tries;
+  trydiv.classList.remove("bg-secondary");
+  if (userNum != winningNum) {
+    div.classList.add("bg-warning");
+  }
   if (userNum < winningNum) {
-    lowerOrHigher.innerText = "too low";
+    lowerOrHigher.innerText = "Too Low";
   } else if (userNum > winningNum) {
-    lowerOrHigher.innerText = "too high";
+    lowerOrHigher.innerText = "Too High";
   } else if (userNum == winningNum) {
     lowerOrHigher.innerText = "Correct!";
-    success = true;
+    div.classList.remove("bg-warning");
+    div.classList.add("bg-success");
   }
+  if (tries < 10) {
+    trydiv.classList.add("bg-success");
+  } else if (tries > 10 && tries < 20) {
+    trydiv.classList.remove("bg-success");
+    trydiv.classList.add("bg-warning");
+  } else if (tries > 20) {
+    trydiv.classList.remove("bg-warning");
+    trydiv.classList.add("bg-danger");
+  }
+  attemptedNums[tries - 1] = userNum;
+
+  attempted.innerText = "";
+  attemptedNums.sort(function (a, b) {
+    return a - b;
+  });
+  for (i = 0; i < attemptedNums.length; i++) {
+    if (i == attemptedNums.length - 1) {
+      attempted.innerText += " " + attemptedNums[i];
+    } else {
+      attempted.innerText += "  " + attemptedNums[i] + ",";
+    }
+  }
+}
+
+function reveal() {
+  document.getElementById("userNum").value = winningNum;
 }
