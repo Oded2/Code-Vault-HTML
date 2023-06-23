6be0eb25c2465383fec5251ddebc36235613dec5
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-06-23 16:21:55 +0300

    Word guesser now works, still a work in progress

--- fun-games/wordguess/script.js
+++ fun-games/wordguess/script.js
@@ -9,27 +9,68 @@ const placeholderFinal = document.getElementById("placeholderFinal");
 const userGuess = document.getElementById("userGuess");
 const checkButton = document.getElementById("checkButton");
 const lettersUsed = document.getElementById("lettersUsed");
-let indexes = [0, 4];
+const hintsDiv = document.getElementById("hintsDiv");
+const definition = document.getElementById("hintDefinition");
+const synonym = document.getElementById("hintSynonym");
+const pos = document.getElementById("hintPoS");
+const definitionDiv = document.getElementById("definitionDiv");
+const synonymDiv = document.getElementById("synonymDiv");
+const posDiv = document.getElementById("posDiv");
+userGuess.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    check();
+  }
+});
 
-// Array of words
+let indexes = [];
+let lettersTried = [];
+let hintsUsed = 0;
 let words;
-getWords();
-function getWords() {
-  fetch("https://random-word-api.herokuapp.com/word?number=100")
-    .then((response) => response.json())
-    .then((data) => {
-      words = data[0];
-      console.log(words);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      return;
-    });
+let hintsData;
+let wordDefinition;
+let wordSynonym;
+let wordAcronym;
+async function getHintInfo(englishWord) {
+  try {
+    const response = await fetch(
+      "https://api.dictionaryapi.dev/api/v2/entries/en/" + englishWord
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    return;
+  }
 }
 
-console.log(words);
+async function getWords() {
+  try {
+    const response = await fetch(
+      "https://random-word-api.herokuapp.com/word?number=10000"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    return;
+  }
+}
 
-function chooseWord() {
+async function fetchDataWord() {
+  words = await getWords();
+}
+
+async function fetchDataHints(wordHint) {
+  hintsData = await getHintInfo(wordHint);
+  let data = hintsData[0];
+  wordDefinition = data["meanings"][0]["definitions"][0]["definition"];
+  // console.log(wordDefinition);
+}
+
+fetchDataWord();
+
+function chooseWordNow() {
   let randomIndex = Math.floor(Math.random() * words.length);
 
   let randomWord = words[randomIndex];
@@ -43,13 +84,34 @@ let word = "cheetah";
 // document.addEventListener("DOMContentLoaded", () => {
 //   titleSection.hidden = true;
 //   gameSection.hidden = false;
+//   hintsDiv.hidden = false;
 // });
 
+function chooseWord(min, max) {
+  for (let i = 0; i < words.length; i++) {
+    let temp = chooseWordNow();
+    if (temp.length >= min && temp.length <= max) {
+      return temp;
+    }
+  }
+}
+
 function startGame() {
-  console.log(words);
-  word = chooseWord();
+  indexes = [];
+  lettersTried = [];
+  hintsUsed = 0;
+  if (hints.checked) {
+    hintsDiv.hidden = false;
+  }
+  definitionDiv.hidden = true;
+  lettersUsed.innerText = "";
+
+  const min = minLen.value;
+  const max = maxLen.value;
+  word = chooseWord(min, max);
+  fetchDataHints(word);
   userGuess.maxLength = word.length;
-  console.log(word);
+  // console.log("Word is: " + word);
   placeholderFinal.innerText = fill(placeholder.value, indexes, word);
   titleSection.hidden = true;
   gameSection.hidden = false;
@@ -57,7 +119,7 @@ function startGame() {
 
 function fill(placeholder, indexes, word) {
   final_word = "";
-  for (let i = 0; i <= word.length; i++) {
+  for (let i = 0; i < word.length; i++) {
     let current = word[i];
     if (indexes.includes(i)) {
       final_word += current;
@@ -70,5 +132,47 @@ function fill(placeholder, indexes, word) {
 
 function test() {
   placeholderFinal.innerText = fill(placeholder.value, indexes, word);
-  console.log(word);
+}
+
+function check() {
+  const user = userGuess.value.toLowerCase();
+  if (user == word) {
+    placeholderFinal.innerText = word;
+  } else {
+    for (let i = 0; i < word.length; i++) {
+      const current = word[i];
+      if (user == current) {
+        indexes[indexes.length] = i;
+      }
+    }
+    if (!word.includes(user)) {
+      if (!lettersTried.includes(user)) {
+        lettersTried[lettersTried.length] = user;
+      }
+      lettersUsed.innerText = "";
+      lettersTried = lettersTried.sort();
+      for (let i = 0; i < lettersTried.length; i++) {
+        if (i == lettersTried.length - 1) {
+          lettersUsed.innerText += " " + lettersTried[i];
+        } else {
+          lettersUsed.innerText += "  " + lettersTried[i] + ",";
+        }
+      }
+    }
+    placeholderFinal.innerText = fill(placeholder.value, indexes, word);
+  }
+  userGuess.value = "";
+  if (placeholderFinal.innerText.toLowerCase() == word) {
+    console.log("You won!");
+  }
+}
+
+function getHint() {
+  definitionDiv.hidden = false;
+  definition.innerText = wordDefinition;
+}
+
+function endGame() {
+  gameSection.hidden = true;
+  titleSection.hidden = false;
 }
