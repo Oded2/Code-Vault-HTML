8d2a35b870fb2077286e29d1b15cf4bbe88378a6
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-06-30 19:21:23 +0300

    Added responsive viewports

--- astrofetch/script.js
+++ astrofetch/script.js
@@ -1,1 +1,17 @@
+const apikey = document.getElementById("apiKey");
 
+async function paste() {
+  const permission = await navigator.permissions.query({
+    name: "clipboard-read",
+  });
+  if (permission.state == "denied") {
+    alert("Clipboard access was denied");
+    return;
+  }
+  const clip = await navigator.clipboard.readText();
+  return clip;
+}
+
+async function pasteToApi() {
+  apikey.value = await paste();
+}
